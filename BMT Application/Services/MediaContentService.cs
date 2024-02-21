d78bddcd7aa6b5c2f40f734c23ace7181c507cbf
BlazorT <info@blazortech.com>
2024-02-21 13:54:20 +0500

    media content page service

--- BMT Application/Services/MediaContentService.cs
+++ BMT Application/Services/MediaContentService.cs
@@ -8,7 +8,6 @@ using com.blazor.bmt.core.interfaces;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using com.blazor.dsps.application.interfaces;
 
 namespace com.blazor.bmt.application.services
 {
