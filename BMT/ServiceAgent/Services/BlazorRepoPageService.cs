1807479fb09f9cf68e690908e1eb449941920621
4dsps <zahid.nawaz@blazortech.com>
2025-08-01 18:21:01 +0500

    Blazor Repo Changes

--- BMT/ServiceAgent/Services/BlazorRepoPageService.cs
+++ BMT/ServiceAgent/Services/BlazorRepoPageService.cs
@@ -281,7 +281,7 @@ namespace Blazor.Web.UI.Services
                     using (var command = connection.CreateCommand())
                     {
                         List<MySqlParameter> parameter = new List<MySqlParameter>();
-                        MySqlParameter pCompaignContentsJSON = new MySqlParameter("p_json", MySqlDbType.LongText, 8000);
+                        MySqlParameter pCompaignContentsJSON = new MySqlParameter("p_json", MySqlDbType.JSON);
                         pCompaignContentsJSON.Value = StoreCompaignContactModelJSON;
                         parameter.Add(pCompaignContentsJSON);
 
