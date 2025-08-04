d04f1f610cd9bee2dafa7d9d152b99323c11db67
arshad172 <arshad.ali@blazortech.com>
2025-08-04 15:43:27 +0500

    NEW

---
+++ BMT/ClientApp/src/configs/ColumnsConfig/recipientsCols.js
@@ -0,0 +1,76 @@
+/* eslint-disable react/react-in-jsx-scope */
+import CampaignActionCell from 'src/components/DataGridCustomCells/CampaignActionCell';
+
+export const getcampaignslistingCols = (getDasList, daRes, pageRoles) => [
+  {
+    field: 'name',
+    headerName: 'Campaign Name',
+    flex: 1,
+    minWidth: 160,
+    editable: false,
+    filterable: true,
+    sortable: true,
+    disableColumnMenu: false,
+    headerClassName: 'custom-header-data-grid',
+  },
+  {
+    field: 'orgName',
+    headerName: 'Organization Name',
+    flex: 1,
+    minWidth: 120,
+    editable: false,
+    filterable: true,
+    sortable: true,
+    disableColumnMenu: true,
+    headerClassName: 'custom-header-data-grid',
+  },
+ 
+ 
+  {
+    field: 'startTime',
+    headerName: 'Start Time',
+    flex: 1,
+    minWidth: 70,
+    // Width: 80,
+    editable: false,
+    filterable: true,
+    sortable: true,
+    disableColumnMenu: true,
+    headerClassName: 'custom-header-data-grid',
+  },
+  {
+    field: 'finishTime',
+    headerName: 'End Time',
+    flex: 1,
+    minWidth: 120,
+    editable: false,
+    filterable: true,
+    sortable: true,
+    disableColumnMenu: true,
+    hide: true,
+    hideable: false,
+    headerClassName: 'custom-header-data-grid',
+  },
+ 
+  {
+    field: 'imageUrl',
+    headerName: 'Action',
+    flex: 1,
+    minWidth: 50,
+    // maxWidth: 100,
+    editable: false,
+    filterable: false,
+    sortable: false,
+    disableColumnMenu: true,
+    headerClassName: 'custom-header-data-grid',
+    renderCell: (params) => (
+      <CampaignActionCell
+        value={params}
+        fetching={daRes}
+        user={daRes.filter((item) => item.id === params.row.id)}
+        canUpdate={pageRoles.canUpdate}
+        canDelete={pageRoles.canDelete}
+      />
+    ),
+  },
+];
