403401c42de3bbe50207f067c8fcb857ae83f4e5
arshad172 <arshad.ali@blazortech.com>
2025-08-04 19:02:30 +0500

    new changes

---
+++ BMT/ClientApp/src/configs/FiltersConfig/recipientsFilterConfig.js
@@ -0,0 +1,63 @@
+// userFormConfig.js
+
+import { cilFlagAlt, cilCalendar, cilUser } from '@coreui/icons';
+import dayjs from 'dayjs';
+import CustomInput from 'src/components/InputsComponent/CustomInput';
+import CustomSelectInput from 'src/components/InputsComponent/CustomSelectInput';
+import CustomDatePicker from 'src/components/UI/DatePicker';
+import globalutil from 'src/util/globalutil';
+import CustomSearch from 'src/components//InputsComponent/CustomSearch';
+
+export const getRecipientsFilterConfig = (filters, changeFilter, orgs) => [
+  {
+    component: CustomSearch,
+    label: 'Organization',
+    value: filters.name,
+    onChange: (e) => changeFilter(e, 'name'),
+    icon: cilUser,
+    type: 'text',
+    id: 'name',
+    data: orgs,
+    name: 'name',
+    placeholder: 'Organization',
+    isRequired: false,
+    title: 'Name Of Organization',
+  },
+  {
+    component: CustomSelectInput,
+    label: 'Networks',
+    value: filters.state,
+    onChange: changeFilter,
+    icon: cilFlagAlt,
+    id: 'networks',
+    name: 'networks',
+    disableOption: 'Select Networks',
+    options: globalutil.networks(),
+    className: 'form-control item form-select',
+    title: 'Networks',
+  },
+  {
+    component: CustomInput,
+    label: 'Recipients',
+    value: filters.tags,
+    onChange: changeFilter,
+    icon: cilFlagAlt,
+    id: 'recipients',
+    name: 'recipients',
+    placeholder: 'recipients e.g (0301300...)',
+    className: 'form-control item',
+    title: 'recipients',
+  },
+  {
+    component: CustomDatePicker,
+    label: 'Date To (Register)',
+    value: filters.lastUpdatedAt,
+    onChange: (e) => changeFilter(e, 'lastUpdatedAt'),
+    icon: cilCalendar,
+   // title: ' DA Registration Date',
+    isRequired: true,
+    min: dayjs(filters.createdAt),
+    message: 'Enter Valid To Date',
+  },
+  
+];
