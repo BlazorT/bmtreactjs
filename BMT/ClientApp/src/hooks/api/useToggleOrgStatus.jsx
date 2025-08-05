4ffd86ddd7871167c9fabadc25d9bdf69db6f0aa
arshad172 <arshad.ali@blazortech.com>
2025-08-05 17:42:15 +0500

    ew

---
+++ BMT/ClientApp/src/hooks/api/useToggleOrgStatus.jsx
@@ -0,0 +1,33 @@
+import moment from 'moment';
+
+import { useShowToast } from '../useShowToast';
+import useApi from '../useApi';
+
+export const useToggleOrgStatus = () => {
+  const { data, error, loading, postData } = useApi('/BlazorApi/adupdateorg');
+
+  const updateStatus = async (user, status) => {
+    const deleteBody = {
+      id: user[0].id,
+      orgId: user[0].orgId === 1 ? 0 : user[0].orgId,
+     
+      status: status,
+      address:"",
+      Contact:"",
+      Email:"",
+      Name:"",
+      createdBy: user[0].createdBy,
+      createdAt: user[0].createdAt,
+      lastUpdatedBy: user[0].lastUpdatedBy,
+      lastUpdatedAt: moment().utc().format(),
+      rowVer: user[0].rowVer,
+    
+    };
+    console.log("deleteBody", deleteBody);
+    const response = await postData(deleteBody);
+    return response;
+  };
+
+
+  return { data, error, loading, updateStatus };
+};
