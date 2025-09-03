67d7ecdb801c00b31c8319f4e3de82218c1c3667
4dsps <zahid.nawaz@blazortech.com>
2025-09-03 12:29:08 +0500

    blazortech ,

---
+++ BMT/ClientApp/src/pages/Auth/Blazorhub.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { HubConnectionBuilder } from '@microsoft/signalr';
+
+class Blazorhub extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nick: '',
+      message: '',
+      messages: [],
+      hubConnection: null,
+    };
+  }
+
+  componentDidMount = () => {
+    const nick = window.prompt('Your name:', 'John');
+
+    const hubConnection = new HubConnectionBuilder().withUrl('/BlazorNotificationHub').build();    
+
+    this.setState({ hubConnection, nick }, () => {
+      this.state.hubConnection
+        .start()
+        .then(() => console.log('Connection started!'))
+        .catch(err => console.log('Error while establishing connection :('));
+
+      this.state.hubConnection.on('BlazorServerMessage', (nick, receivedMessage) => {
+        const text = `${nick}: ${receivedMessage}`;
+        const messages = this.state.messages.concat([text]);
+        this.setState({ messages });
+      });
+    });
+  };
+
+  sendMessage = () => {
+    this.state.hubConnection
+      .invoke('BlazorServerMessage', this.state.nick, this.state.message)
+      .catch(err => console.error(err));
+
+      this.setState({message: ''});      
+  };
+
+  render() {
+    return (
+      <div>
+        <br />
+        <input
+          type="text"
+          value={this.state.message}
+          onChange={e => this.setState({ message: e.target.value })}
+        />
+
+        <button onClick={this.sendMessage}>Send</button>
+
+        <div>
+          {this.state.messages.map((message, index) => (
+            <span style={{display: 'block'}} key={index}> {message} </span>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Blazorhub;
