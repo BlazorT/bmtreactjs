ec1bc3fa6230fdd436aabccb6c4941e4776ff488
Adil553 <adil.waheed@blazortech.com>
2026-02-12 18:42:21 +0500

    Refactor pricing and recipients column configurations to remove redundant properties and improve code clarity. Adjusted the renderCell function for recipients to streamline the component rendering process.

--- BMT/ClientApp/src/configs/ColumnsConfig/recipientsCols.js
+++ BMT/ClientApp/src/configs/ColumnsConfig/recipientsCols.js
@@ -1,51 +1,39 @@
+/* eslint-disable react/react-in-jsx-scope */
 // src/configs/ColumnsConfig/recipientsCols.js
-import React from 'react';
 import RecipientsActionCell from 'src/components/DataGridCustomCells/RecipientsActionCell';
 
 export const getrecipietslistingCols = (pageRoles) => [
   {
     key: 'networkId',
     name: 'Network Name',
-<<<<<<< HEAD
-=======
     editable: false,
     filterable: true,
     flex: 1,
     disableColumnMenu: false,
->>>>>>> 0879477860104145becebbb26176bac821469060
   },
   {
     key: 'albumid',
     name: 'Album',
-<<<<<<< HEAD
-=======
     editable: false,
     filterable: true,
     flex: 1,
     disableColumnMenu: false,
->>>>>>> 0879477860104145becebbb26176bac821469060
   },
   {
     key: 'contentId',
     name: 'Recipients',
-<<<<<<< HEAD
-=======
     editable: false,
     filterable: true,
     flex: 1,
     disableColumnMenu: true,
->>>>>>> 0879477860104145becebbb26176bac821469060
   },
   {
     key: 'createdAt',
     name: 'Created At',
-<<<<<<< HEAD
-=======
     editable: false,
     flex: 1,
     filterable: true,
     disableColumnMenu: true,
->>>>>>> 0879477860104145becebbb26176bac821469060
   },
   {
     key: 'action',
@@ -54,11 +42,6 @@ export const getrecipietslistingCols = (pageRoles) => [
     filterable: false,
     disableColumnMenu: true,
     width: 120,
-    renderCell: (params) => (
-      <RecipientsActionCell
-        row={params.row}
-        pageRoles={pageRoles}
-      />
-    ),
+    renderCell: (params) => <RecipientsActionCell row={params.row} pageRoles={pageRoles} />,
   },
 ];
